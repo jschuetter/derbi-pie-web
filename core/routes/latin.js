56db19ebd2834457f3057f85b6e4acd3fa6ac41e
jschuetter <100084569+jschuetter@users.noreply.github.com>
2025-11-01 19:54:39 -0400

    Fixed content tweaks

--- core/routes/latin.js
+++ core/routes/latin.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
+    //Check for search query from search bar?
+
+    // Check for results queryparams when rendering?
     res.render('latin');
 });
 
